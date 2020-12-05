a46d1772a9890a601c9c775e581281677e6db9e6
Anthony Sottile <asottile@umich.edu>
2020-12-04 21:14:04 -0800

    add helper downloader tool for input

--- support/support.py
+++ support/support.py
@@ -1,5 +1,8 @@
+import argparse
 import contextlib
 import time
+import urllib.error
+import urllib.request
 from typing import Generator
 
 
@@ -18,3 +21,35 @@ def timing(name: str = '') -> Generator[None, None, None]:
         if name:
             name = f' ({name})'
         print(f'> {int(t)} {unit}{name}')
+
+
+def get_input(year: int, day: int) -> str:
+    with open('../.env') as f:
+        contents = f.read()
+
+    url = f'https://adventofcode.com/{year}/day/{day}/input'
+    req = urllib.request.Request(url, headers={'Cookie': contents.strip()})
+    return urllib.request.urlopen(req).read().decode()
+
+
+def download_input() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('year', type=int)
+    parser.add_argument('day', type=int)
+    args = parser.parse_args()
+
+    for i in range(5):
+        try:
+            s = get_input(args.year, args.day)
+        except urllib.error.URLError as e:
+            print(f'zzz: not ready yet: {e}')
+            time.sleep(1)
+        else:
+            break
+    else:
+        raise SystemExit('timed out after attempting many times')
+
+    with open('input.txt', 'w') as f:
+        f.write(s)
+
+    return 0
